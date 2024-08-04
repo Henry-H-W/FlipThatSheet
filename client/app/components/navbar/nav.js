fedd4a60096dfb5eeb67221d7e09d4e22baacad3
derektan12 <derektan101@gmail.com>
2024-08-04 02:46:00 -0400

    q

--- client/app/components/navbar/nav.js
+++ client/app/components/navbar/nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { styled } from '@mui/system';
 import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0/client';
 import { NavButton, AuthButton, Logo } from './styles';
 
 const NavbarContainer = styled(Box)({
@@ -28,27 +29,44 @@ const RightSection = styled(Box)({
 });
 
 const Navbar = () => {
-    return (
-      <NavbarContainer>
-        <LeftSection>
-          <Logo src="/music.svg" onClick={() => window.location.href = '/'} />
-          <Link href="/music" passHref>
-            <NavButton component="a">Music</NavButton>
-          </Link>
-          <Link href="/recordings" passHref>
-            <NavButton component="a">Recordings</NavButton>
-          </Link>
-        </LeftSection>
-        <RightSection>
-          <Link href="/login" passHref>
-            <AuthButton component="a">Login</AuthButton>
-          </Link>
-          <Link href="/signup" passHref>
-            <AuthButton component="a" variant="contained" color="secondary">Sign Up</AuthButton>
-          </Link>
-        </RightSection>
-      </NavbarContainer>
-    );
+  const { user, isLoading } = useUser();
+
+  const handleLogin = () => {
+    // Redirect to Auth0 login page
+    window.location.href = '/api/auth/login';
   };
-  
-  export default Navbar;
+
+  const handleLogout = () => {
+    // Redirect to Auth0 logout page
+    window.location.href = '/api/auth/logout';
+  };
+
+  if (isLoading) return null; // Optionally render a loading state
+
+  return (
+    <NavbarContainer>
+      <LeftSection>
+        <Logo src="/music.svg" onClick={() => window.location.href = '/'} />
+        <Link href="/music" passHref>
+          <NavButton component="a">Music</NavButton>
+        </Link>
+        <Link href="/recordings" passHref>
+          <NavButton component="a">Recordings</NavButton>
+        </Link>
+      </LeftSection>
+      <RightSection>
+        {!user ? (
+          <AuthButton component="a" variant="contained" color="secondary" onClick={handleLogin}>
+            Login
+          </AuthButton>
+        ) : (
+          <AuthButton component="a" onClick={handleLogout}>
+            Log Out
+          </AuthButton>
+        )}
+      </RightSection>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
